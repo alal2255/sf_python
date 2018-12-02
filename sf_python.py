9e408abbb320635bbd59023058e0270e6f8ab41a
ASHISH <LAL@ASHISHs-MacBook-Air.local>
2018-12-02 15:10:10 -0500

    Saleforce Security Logs using simple_salesforce

---
+++ sf_python.py
@@ -0,0 +1,25 @@
+from simple_salesforce import Salesforce
+import json,io
+import pprint
+
+
+#list []
+sf=Salesforce(username='aglal@icf.com',password='Therock01',security_token='YsWyF2LW6B2i83kA6bieuEXg')
+#sf=Salesforce(instance_url='https://na85.salesforce.com',session_id='00D1U000000EVj3!AQ8AQMfhARMOhePr4kalZYFERLyRC5juwpqtZ0LiKuHiE57bckDLEyjE5FKXHs8P.bXpnr9Isv6_p7HjAKv8j32vAQvUy4V0') ##Salesforce OAuth Connection ###
+r1 = sf.query("SELECT SourceIp from LoginIp") ##Salesforce SoSQL Query###
+r2 = sf.query("SELECT Country from LoginGeo Limit 10")
+r3 = sf.
+
+r3 = [r1,r2]
+#def merge(dict1,dict2):
+#    dict3 = dict1.copy()
+#    dict3.update(dict2)
+    #res = {**dict1,**dict2}
+#    return dict3
+#dict1 = r1
+#dict2 = r2
+#dict3 = merge(r1,r2)
+#print(dict3)
+for obj in r3:
+    print(json.dumps(obj,skipkeys='True',indent=4))
+
