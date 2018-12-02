0b773a2a6d62340673a4864c990d5b6e97afa422
Ashish <ashish.lal@outlook.com>
2018-12-02 18:39:38 -0500

    Access Login Info

--- sf_login.py
+++ sf_login.py
@@ -3,11 +3,11 @@ class sf_log:
         self.username = username
         self.passcode = passcode
 
-uname = input("Enter Username:")
-pass1 = input("Enter Passcode:")
+#uname = input("Enter Username:")
+#pass1 = input("Enter Passcode:")
 
-p1 = sf_log(uname,pass1)
-print(p1.username)
-print(p1.passcode)
+#p1 = sf_log(uname,pass1)
+#print(p1.username)
+#print(p1.passcode)
     
 
--- sf_python.py
+++ sf_python.py
@@ -4,14 +4,15 @@ import json,io
 import pprint
 
 
-#list []
-sf=Salesforce(username='xxxxx@xxxxx.com',password=Salesforceaccountpasscode,security_token=securitotken)
+uname = input("Enter Username:")
+pass1 = input("Enter Passcode:")
+p1 = sf_log(uname,pass1)
+
+#sf=Salesforce(username='xxxxx@xxxxx.com',password=Salesforceaccountpasscode,security_token=securitotken)
 #sf=Salesforce(instance_url='https://na85.salesforce.com',session_id='00D1U000000EVj3!AQ8AQMfhARMOhePr4kalZYFERLyRC5juwpqtZ0LiKuHiE57bckDLEyjE5FKXHs8P.bXpnr9Isv6_p7HjAKv8j32vAQvUy4V0') ##Salesforce OAuth Connection ###
-r1 = sf.query("SELECT SourceIp from LoginIp") ##Salesforce SoSQL Query###
-r2 = sf.query("SELECT Country from LoginGeo Limit 10")
-
-
-r3 = [r1,r2]
+#r1 = sf.query("SELECT SourceIp from LoginIp") ##Salesforce SoSQL Query###
+#r2 = sf.query("SELECT Country from LoginGeo Limit 10")
+#r3 = [r1,r2]
 #def merge(dict1,dict2):
 #    dict3 = dict1.copy()
 #    dict3.update(dict2)
@@ -21,6 +22,6 @@ r3 = [r1,r2]
 #dict2 = r2
 #dict3 = merge(r1,r2)
 #print(dict3)
-for obj in r3:
-    print(json.dumps(obj,skipkeys='True',indent=4))
+#for obj in r3:
+#    print(json.dumps(obj,skipkeys='True',indent=4))
 
